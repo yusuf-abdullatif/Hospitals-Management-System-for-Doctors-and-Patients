144d81b0cca99dd8a844e597c0fc96cb70efd909
Ahyildiz <ahyildiz15@gmail.com>
2023-11-13 17:02:30 +0300

    created template for project and added icon

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-﻿const loginButton = document.querySelector('.login-button');
+const loginButton = document.querySelector('.login-button');
 const loginPopup = document.querySelector('.login-popup');
 const loginPopupClose = document.querySelector('.login-popup-close');
-document.querySelector('.homebutton');
+
 loginButton.addEventListener('click', () => {
     loginPopup.style.display = 'block';
 });
@@ -9,60 +9,3 @@ loginButton.addEventListener('click', () => {
 loginPopupClose.addEventListener('click', () => {
     loginPopup.style.display = 'none';
 });
-// Initialize and add the map
-let map;
-
-async function initMap() {
-    // The location of Uluru
-    const position = { lat: -25.344, lng: 131.031 };
-    // Request needed libraries.
-    //@ts-ignore
-    const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-    // The map, centered at Uluru
-    map = new Map(document.getElementById("map"), {
-        zoom: 4,
-        center: position,
-        mapId: "DEMO_MAP_ID",
-    });
-
-    // The marker, positioned at Uluru
-    const marker = new AdvancedMarkerElement({
-        map: map,
-        position: position,
-        title: "Uluru",
-    });
-}
-
-initMap();
-// The location of Uluru
-const position = { lat: -25.344, lng: 131.031 };
-// Request needed libraries.
-//@ts-ignore
-const { Map } = await google.maps.importLibrary("maps");
-
-
-// The map, centered at Uluru
-map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-});
-function msgAlert() {
-    const msgSent = "Message Sent!";
-    alert(msgSent);
-}
-
-function openLoginModal() {
-    document.getElementById('loginModal').style.display = 'flex';
-}
-
-function closeLoginModal() {
-    document.getElementById('loginModal').style.display = 'none';
-}
-
-var doctorname = "Dr. Baris Can Kara"; //temporary
-var patientname = "Patient"; //temporary
-var elementdashboard = document.getElementById("doctor-info");
-elementdashboard.innerHTML = "Welcome " + doctorname + "!";
