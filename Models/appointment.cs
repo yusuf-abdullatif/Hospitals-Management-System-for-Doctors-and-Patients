7309bca7503e6952af148ce9e62edc027d9d6834
Ahyildiz <ahyildiz15@gmail.com>
2023-12-28 01:07:46 +0300

    Revert "working on setting foreign keys"
    This reverts commit 401ffe7d9c19cfb6d4a9e6a9fc37e92b191f74b6.

--- Models/appointment.cs
+++ Models/appointment.cs
@@ -9,16 +9,12 @@ namespace WebProject.Models
 {
     public class appointment
     {
-         // This is the navigation property for doctor
         [Key] // This attribute marks the property as the primary key
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // This attribute specifies that the database should generate a value for this property
         public virtual int appointmentID {get;set;}
         public virtual string date {get;set;} //Datetime?
-        [ForeignKey("doctorID")]
-        public virtual doctor Doctor { get; set; }
-        [ForeignKey("patientID")]
-        public virtual patient Patient { get; set; }
-
+        public virtual int doctorID {get;set;}
+        public virtual int patientID {get;set;}
         public virtual string hospitalName { get; set; }
     }
 }
