401ffe7d9c19cfb6d4a9e6a9fc37e92b191f74b6
Baran Beytemür <baran.beytemur@hotmail.com>
2023-12-28 00:31:13 +0300

    working on setting foreign keys

---
+++ Migrations/202312271950463_AddForeignKeys.cs
@@ -0,0 +1,24 @@
+﻿namespace WebProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddForeignKeys : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.appointments", "doctorID");
+            CreateIndex("dbo.appointments", "patientID");
+            AddForeignKey("dbo.appointments", "doctorID", "dbo.doctors", "doctorID", cascadeDelete: true);
+            AddForeignKey("dbo.appointments", "patientID", "dbo.patients", "patientID", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.appointments", "patientID", "dbo.patients");
+            DropForeignKey("dbo.appointments", "doctorID", "dbo.doctors");
+            DropIndex("dbo.appointments", new[] { "patientID" });
+            DropIndex("dbo.appointments", new[] { "doctorID" });
+        }
+    }
+}
