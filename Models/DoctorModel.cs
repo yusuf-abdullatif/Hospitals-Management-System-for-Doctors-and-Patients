658824e6093ce711cee392afa507a5dbced7f94c
Ahyildiz <ahyildiz15@gmail.com>
2023-12-28 02:23:07 +0300

    first update

---
+++ Models/DoctorModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebProject.Models
+{
+    public class DoctorModel
+    {
+        [Required(ErrorMessage = "Please enter yor Username")]
+        public string DoctorUsername { get; set; }
+        [Required(ErrorMessage = "Please enter your password")]
+        public string DoctorPassword { get; set; }
+        [Required(ErrorMessage = "Please enter your name")]
+        public string DoctorName { get; set; }
+        [Required(ErrorMessage = "Please enter your Email")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string DoctorEmail { get; set; }
+    }
+}
---
+++ Models/PatientModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebProject.Models
+{
+    public class PatientModel
+    {
+        [Required(ErrorMessage = "Please enter your SSN")]
+        public int PatientSsn { get; set; }
+        [Required(ErrorMessage = "Please enter your name")]
+        public string PatientName { get; set; }
+        public string PatientAddress { get; set; }
+        public string PatientPhone { get; set; }
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string PatientEmail { get; set; }
+        [Required(ErrorMessage = "Please enter your password")]
+        public string PatientPassword { get; set; }
+
+    }
+}
